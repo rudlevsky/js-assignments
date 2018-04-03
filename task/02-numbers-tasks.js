d82669d9c537c084239d0d31713067c3fb1f40b3
Ilya <rudlevsky@gmail.com>
2018-04-03 21:12:06 +0300

    two units completed

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -54,7 +54,9 @@ function getCicleCircumference(radius) {
  *  -3, 3  => 0
  */
 function getAverage(value1, value2) {
-    throw new Error('Not implemented');
+    value1 = value1/2;
+	value2 = value2/2;
+	return value1 + value2;
 }
 
 /**
@@ -111,7 +113,7 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (1,2)     => 0
  */
 function getAngleBetweenVectors(x1, y1, x2, y2) {
-    throw new Error('Not implemented');
+	return Math.acos((x1*x2 + y1*y2)/(Math.sqrt(x1*x1 + y1*y1) * Math.sqrt(x2*x2 + y2*y2)));
 }
 
 /**
